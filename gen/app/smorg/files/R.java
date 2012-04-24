1d851528ae1a288678e27a5d29cf428f80db0e40
Tarek Assem <tarek.assemgamal@gmail.com>
2012-04-24 23:59:18 +0200

    splash layout made

--- gen/app/smorg/files/R.java
+++ gen/app/smorg/files/R.java
@@ -13,8 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int appName=0x7f050000;
+        public static final int b_signIn=0x7f050003;
+        public static final int b_signUp=0x7f050004;
+        public static final int username=0x7f050001;
+        public static final int userpw=0x7f050002;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int splash=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
